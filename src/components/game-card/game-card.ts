4d6ab8b3765bc0bf3beea4618003992e16f917f2
Doug Rudolph <drudolph914@gmail.com>
2024-07-09 00:07:26 -0400

    fix: adding suit and rank props to game-card

--- src/components/game-card/game-card.ts
+++ src/components/game-card/game-card.ts
@@ -18,6 +18,8 @@ export class GameCard extends LitElement {
     isFaceShowing: { type: Boolean },
     isHidden: { type: Boolean},
     isEmpty: { type: Boolean },
+    suit: { type: String },
+    rank: { type: String }
   }
 
   gridX: number
@@ -26,6 +28,8 @@ export class GameCard extends LitElement {
   isFaceShowing: boolean
   isHidden: boolean
   isEmpty: boolean
+  suit: string
+  rank: string
 
   constructor() {
     super()
@@ -36,6 +40,8 @@ export class GameCard extends LitElement {
     this.isFaceShowing = true
     this.isHidden = false
     this.isEmpty = true
+    this.suit = ''
+    this.rank = ''
   }
 
   render() {
@@ -47,6 +53,10 @@ export class GameCard extends LitElement {
 
     if (this.isEmpty) {
       classMap += ' empty'
+    }
+
+    if (!this.isFaceShowing) {
+      classMap += ' card-back'
     }
 
     return html`
